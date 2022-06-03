f6e0d7165eaf726c8445ef58e1b8dc776160caa7
dj-pmfst <100929210+dj-pmfst@users.noreply.github.com>
2022-06-03 12:29:00 +0200

    Update zadatak2.py

--- vjezbe/vjezbe4/zadatak2.py
+++ vjezbe/vjezbe4/zadatak2.py
@@ -4,24 +4,27 @@ import math
 import matplotlib.pyplot as plt
 
 def f1(x):
-    return x*x
+    return x**2
 
 N_lista = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
 koraci = []
 trapezni = []
 gornja = []
 donja = []
+analiticko = []
 for N in N_lista:
     koraci.append(N)
-    gornja_s, donja_s = calculus.integral(f1, 0, 2, N)
-    gornja.append(gornja_s)
-    donja.append(donja_s)
+    #gornja_s, donja_s = calculus.integral(f1, 0, 2, N)
+    gornja.append(calculus.integral(f1, 0, 2, N)[0])
+    donja.append(calculus.integral(f1, 0, 2, N)[1])
     trapezni.append(calculus.integral_t(f1, 0, 2, N))
+    analiticko.append(8/3)
     
-plt.plot(koraci, [8/3]*len(N_lista), color="black")
-plt.ylim(2.663, 2.68)
-plt.xlim(10, 900)
+#plt.plot(koraci, [8/3]*len(N_lista), color="black")
+#plt.ylim(2.663, 2.68)
+#plt.xlim(10, 900)
 plt.scatter(koraci, gornja, color="red")
 plt.scatter(koraci, donja, color="yellow")
 plt.scatter(koraci, trapezni)
+plt.plot(koraci, analiticko)
 plt.show()
