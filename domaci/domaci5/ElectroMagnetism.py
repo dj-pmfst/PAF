06fbb23e549cd7ae814425aef8dce8002f2d42b2
dj-pmfst <100929210+dj-pmfst@users.noreply.github.com>
2022-06-05 22:48:43 +0200

    Add files via upload

---
+++ domaci/domaci5/ElectroMagnetism.py
@@ -0,0 +1,68 @@
+import math
+import numpy as np 
+import matplotlib.pyplot as plt
+
+class ElectroMagnetism():
+    def __init__(self, v0, polozaj, q, E, B, m, t):
+        self.m = m
+        self.dt = 0.01
+        #self.t = np.linspace(0, t, int(t/self.dt))
+        self.p = [polozaj]
+        self.v = [v0]
+        self.q = q
+        self.E = E
+        self.B = B
+        self.t = [0]
+        self.t_uk = t
+        self.a =[q*(E + np.cross(v0, self.__B_t(self.t[-1])))/m]
+        
+    def move_e(self):
+        for i in self.t:
+            self.v.append(self.v[-1] + self.a[-1] * self.dt)
+            self.p.append(self.p[-1] + self.v[-1] * self.dt)
+            self.a.append(self.q*(self.E + np.cross(self.v[-1], self.B))/self.m)
+    
+    def plot(self):
+        x = []
+        y = []
+        z = []
+        for i in range(len(self.p)):
+            x.append(self.p[i][0])
+            y.append(self.p[i][1])
+            z.append(self.p[i][2])
+        return x, y, z
+    
+    def __acc(self, v, i):
+        return self.q/self.m * (self.E + np.cross(v, self.__B_t(i)))
+    
+    def move_rk(self):
+        x = [0]
+        y = [0]
+        z = [0]
+        while self.t[-1] <= self.t_uk:
+            v1 = self.__acc(self.v[-1], self.t[-1]) * self.dt
+            p1 = self.v[-1] * self.dt
+
+            v2 = self.__acc(self.v[-1] + v1 / 2, self.t[-1]) * self.dt
+            p2 = (self.v[-1] + p1 / 2) * self.dt
+
+            v3 = self.__acc(self.v[-1] + v2 / 2, self.t[-1]) * self.dt
+            p3 = (self.v[-1] + p2 / 2) * self.dt
+
+            v4 = self.__acc(self.v[-1] + v3, self.t[-1]) * self.dt
+            p4 = (self.v[-1] + p3) * self.dt
+
+            self.v.append(self.v[-1] + (1 / 6) * (v1 + 2 * v2 + 2 * v3 + v4))
+
+            x.append(x[-1] + (p1[0] + 2*p2[0] + 2*p3[0] + p4[0])/6)
+            y.append(y[-1] + (p1[1] + 2*p2[1] + 2*p3[1] + p4[1])/6)
+            z.append(z[-1] + (p1[2] + 2*p2[2] + 2*p3[2] + p4[2])/6)
+
+            self.t.append(self.t[-1] + self.dt)
+
+            #print(self.__B_t(i))
+        
+        return x, y, z
+    
+    def __B_t(self, t):
+        return np.array((0,0,t/10))
